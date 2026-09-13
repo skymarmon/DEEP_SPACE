9a8de4cb2cecb3d602233843addd1c16599c2a89
skymarmon <108655366+skymarmon@users.noreply.github.com>
2026-09-13 13:48:40 +0900

    Refactor lobby.js to export startLobby function

--- script/lobby.js
+++ script/lobby.js
@@ -1,16 +1,21 @@
-const game = document.getElementById("game");
+```javascript
+export function startLobby() {
 
-game.innerHTML = `
-    <div style="
-        width: 100%;
-        height: 100%;
-        background: #202020;
-        color: white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 40px;
-    ">
-        LOBBY
-    </div>
-`;
+    const game = document.getElementById("game");
+
+    game.innerHTML = `
+        <div style="
+            width: 100%;
+            height: 100%;
+            background: #202020;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 40px;
+        ">
+            LOBBY
+        </div>
+    `;
+}
+```
