dc46b92570ac0fc2b193537bf6ceb84e89133739
skymarmon <108655366+skymarmon@users.noreply.github.com>
2026-09-13 13:35:19 +0900

    Add lobby.js to display lobby interface

---
+++ script/lobby.js
@@ -0,0 +1,16 @@
+const game = document.getElementById("game");
+
+game.innerHTML = `
+    <div style="
+        width: 100%;
+        height: 100%;
+        background: #202020;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 40px;
+    ">
+        LOBBY
+    </div>
+`;
